b8b18c685b52f0e519712ace8665f127c845b19f
rheasarin <82701405+rheasarin@users.noreply.github.com>
2022-01-23 13:57:46 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# c28class
+# AngryBirdsStage3
+Stage 3 Angry Birds: Introducing Constraint
---
+++ Slingshot.js
@@ -0,0 +1,25 @@
+class SlingShot{
+    constructor(bodyA, bodyB){
+        var options = {
+            bodyA: bodyA,
+            bodyB: bodyB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.sling.bodyB.position;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+       
+    }
+   fly(){
+       this.sling.bodyA=null;
+   }
+}
